fbb44dd52c814a93dcf4c8bf01cc1024ba16c7ca
GannJobs <linksyamato@gmail.com>
2022-05-14 17:35:11 -0300

    Tips e Treino Commit direto no Eclipse

---
+++ src/br/com/dio/PPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+public class PPrograma {
+
+	public static void main(String[] args) {
+
+		// sys crtl + space;
+		System.out.println("Hello Mother Fucker!");
+		// Ctrl m pra aumentar
+		// Ctrl 3 gerar lupa para usar modelos pre montados como construtors, getters,
+		// setters, ToString, Equals, Hashcode
+		// Crtl d apaga linha
+		// Crtl + Shift + o importa a classe que está ultilizando
+		// Team - share project cria repositorio, depois de criado va denovo e de push
+	}
+
+}
